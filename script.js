380bf07c2147a6625156884b3215c945a31b90a9
Keegan Nair <Keegan.nair@hotmail.com>
2021-01-24 13:08:49 +1100

    Had to rename most of the ID and class names, and removed majority of commented code

--- script.js
+++ script.js
@@ -1,171 +1,59 @@
-//var timerElement = document.getElementById('timer');
+$(document).ready(function () {
+  // listen for save button clicks
+  $(".saveBtn").on("click", function () {
+    // get nearby values
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
+    // save in localStorage
+    localStorage.setItem(time, value);
+  });
 
+  function hourUpdater() {
+    // get current number of hours
+    var currentHour = moment().hours();
 
-var m = moment(); //makes it easier to type this 
+    // loop over time blocks
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-//console.log(moment().toString());
-//console.log(moment().getDate);
-
-//console.log(m.format("dddd MMM Mo YYYY"));
-
-$("#date").html(m.format("dddd, Do MMM YYYY")); //sets the date using the Moment.js library
-
-
-//$("#test").text(m.format("hh:mm A"));
-$("#currentTime").text(m.format("hh:mm A"));
-
-
-//$(".time9").text(m.format("9", "hh:mm A") + "AM");
-
-
-// changeColor();
-
-// function changeColor(){
-//   if(m == m.hour(12)){
-//     $("#test").css("color", "red");
-//    // $("p").css("color", "red");
-//   }else if(m == 14){
-//     $("#test").css("color", "blue");
-//   }
-
-// }
-
-
-displayTasks();
-
-
-//console.log( moment('Mon 03-Jul-2017, 11:00 AM', 'ddd DD-MMM-YYYY, hh:mm A').format('hh:mm A') );
-
-
-
-//moment("Apr 4 05:06:07", "MMM DD hh:mm:ss");  // this year, 4th April, 05:06:07.000
-
-
-
-// var beginningTime = moment('8:45am', 'h:mma');
-// var endTime = moment('9:00am', 'h:mma');
-// console.log(beginningTime.isBefore(endTime)); // true
-// console.log(beginningTime.toDate()); // Mon May 12 2014 08:45:00
-// console.log(endTime.toDate()); // Mon May 12 2014 09:00:00
-
-
-//var saveBtn = $("#saveBtn"); //not doing anthing
-//saveBtn.append(newButton);
-
-//the following code makes sure that the save button is clicked
-$(".saveBtn").on("click", function() {
-  var savedTasks = [];
-
-  console.log("button pressed");
-  //var todoItem = "test";
-  var todoItem  = $(this).attr("data-time");
-  console.log(todoItem);
-  var textItem = $("#" + todoItem).val();
-  savedTasks.push(textItem);
-
-  console.log(savedTasks);
-  var storedTasks = localStorage.getItem("storedTasks");
-
-  if(storedTasks == null){
-    //this will take the first input and put it into the array since array is emtpy
-    localStorage.setItem("storedTasks", JSON.stringify(savedTasks));
-    $("#" + todoItem).val("");
-  }else{
-    //this will add other inputs inside the same array
-    storedTasks = JSON.parse(storedTasks);
-    console.log(storedTasks)
-    storedTasks.push(textItem);
-    localStorage.setItem("storedTasks", JSON.stringify(storedTasks));
-    $("#" + todoItem).val("");
+      // check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      }
+      else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      }
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
   }
 
-  localStorage.setItem(todoItem, textItem);  //should store input into the local stoarge. with time
-  displayTasks();
+  hourUpdater();
+
+  // set up interval to check if current time needs to be updated
+  var interval = setInterval(hourUpdater, 15000);
+
+  // load any saved data from localStorage
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+  // display current day on page
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+//   $("#date").html(m.format("dddd, Do MMM YYYY")); //sets the date using the Moment.js library
+
+// //$("#test").text(m.format("hh:mm A"));
+// $("#currentTime").text(m.format("hh:mm A"));
 });
-
-function displayTasks(){
-  // var tasksFromLocalStorage = JSON.parse(localStorage.getItem("storedTasks"));
-  var times = ['11am', '10am', "9am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
-  // if(tasksFromLocalStorage != null){
-    for(var i=0; i<times.length; i++){
-      var task = (localStorage.getItem(times[i]));
-      $("#"+times[i]).val(task)
-  //     var textItem = tasksFromLocalStorage[i];
-
-  //     var todoItem  = $(this).attr("data-time");
-      
-  //    // var textItem = $("#" + todoItem).val();
-  //    // savedTasks.push(textItem);
-
-  //  //  var oldItems = JSON.parse(localStorage.getItem('savedTasks')) || [];
-  //    //tasksFromLocalStorage.push(textItem);
-
-
-  //     $("#"+ i + todoItem).append(textItem);
-  //     console.log(textItem);
-    }
-  // }
-
-}
-
-//when saving store todo item as key and textItem as value
-//when displaying, loop through all object(inside local storage array)
-//assign key and value to correct input
-
-//[{key:"9am", value:"work"},{key:"3pm", value:"leave work"}]
-
-//$(key).val(value);
-
-
-
-
-
-
-//localStorage.setItem("Name", "Keegan");
-
-//this.attr or this.data
-
-
-//queryselector
-//document.querySelector("#mytextinput").textContent
-
-
-// function action() {
-//   window.localStorage['todo-text'] = document.getElementById('todo-text').value;
-// }
-
-// $('#saveBtn').on('click', function(){
-
-//   $('input[type="text"]').each(function(){    
-//       var id = $(this).attr('id');
-//       var value = $(this).val();
-//      localStorage.setItem(id, value);
-
-//   });   
-// });
-
-
-//How do i have multilple saves that all click? rn just have one that can click, shows result in console.log.
-//can savebtn be universal?
-
-//how do I get the time to compare with moment.js?
-
-//refer to activity 8 line 40
-
-
-//register click event on all btn class (all btns have same class)
-//link the button to its respective input and get the text
-//once you have value, save it to local storage
-//see if you can change this data type to be array of object
-//stringify whike saving parse when extracting
-
-
-// $(".yourButtonClass").on('click', function(event){
-//   event.stopPropagation();
-//   event.stopImmediatePropagation();
-//   //(... rest of your JS code)
-// });
-
-
-
